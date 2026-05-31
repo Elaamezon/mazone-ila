8a58d4685868f20a06c0031691f607398fcfe528
Elaamezon <mezonelaayazd1344@gmail.com>
2026-05-31 09:48:56 -0800

    Update server.js

--- server.js
+++ server.js
@@ -149,3 +149,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("🚀 Server running on port " + PORT);
 });
+let supports = [];
+
+app.post("/support", (req,res)=>{
+  supports.push(req.body);
+  res.json({success:true});
+});
