79f8f03ef8048cf9b9d681df10e4d86a12dd5fde
Elaamezon <mezonelaayazd1344@gmail.com>
2026-05-31 06:16:49 -0800

    server.js

--- server.js
+++ server.js
@@ -154,6 +154,8 @@ app.delete("/delete-order/:id", (req, res) => {
 /* =========================
    🚀 اجرا
 ========================= */
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
